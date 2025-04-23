cc6084fe0b11efa5c658d65b9c89ee774f0ff56e
Daniel Abel <daniel@zkreations.com>
2025-04-23 18:25:13 -0500

    feat: add icons.config.js for SVG configuration

---
+++ build/icons.config.js
@@ -0,0 +1,26 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default {
+  iconsDir: path.join(__dirname, '../icons/'),
+  outputDir: path.join(__dirname, '../dist/'),
+
+  spriteFilename: 'icons.svg',
+  includableFilename: 'icons.xml',
+  jsonFilename: 'icons.json',
+  jsFilename: 'icons.js',
+
+  defaultSvgAttributes: {
+    class: '',
+    viewBox: '0 0 24 24',
+    xmlns: 'http://www.w3.org/2000/svg',
+    fill: 'none',
+    stroke: 'currentColor',
+    'stroke-width': '2',
+    'stroke-linecap': 'round',
+    'stroke-linejoin': 'round'
+  }
+}
