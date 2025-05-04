908b370a0508801f9e365fdefb280c2413e698ed
Daniel Abel <daniel@zkreations.com>
2025-05-04 14:40:15 -0500

    refactor: add removeAttrs plugin to remove unnecessary attributes

--- build/svgo.config.js
+++ build/svgo.config.js
@@ -7,6 +7,12 @@ export default {
   },
   plugins: [
     {
+      name: 'removeAttrs',
+      params: {
+        attrs: 'xmlns'
+      }
+    },
+    {
       name: 'preset-default',
       params: {
         overrides: {
